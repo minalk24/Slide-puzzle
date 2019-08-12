3cd011160b02b8b50f31e64223cf52ac05d73e7b
Minal Kharche <minal.berkeley@gmail.com>
2019-08-12 14:07:54 -0700

    added initialize function and update json

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,3 +12,40 @@ emptyObj.isEmptyTileAvailAdjacent = false;
 emptyObj.clickIndex = [];
 emptyObj.emptyTiles = [];
 emptyObj.clickedValue = 0;
+
+
+    //initialize 
+    initialze();
+
+    //update Json
+    function updateInitialValuesObject(){
+        //chanage to 9
+        var index = _.findIndex(emptyObj.initialValues, { 'row' : parseInt(emptyObj.clickIndex[0]), 'column' : parseInt(emptyObj.clickIndex[1])});
+        emptyObj.initialValues[index] = {"row":parseInt(emptyObj.clickIndex[0]) , "column":parseInt(emptyObj.clickIndex[1]) , "value":9, "hidden":true};
+        
+        //chanage to 9
+        var index = _.findIndex(emptyObj.initialValues, { 'row' : parseInt(emptyObj.emptyTiles[0]), 'column' : parseInt(emptyObj.emptyTiles[1])});
+        emptyObj.initialValues[index] = {"row":parseInt(emptyObj.emptyTiles[0]) , "column":parseInt(emptyObj.emptyTiles[1]) , "value": parseInt(emptyObj.clickedValue), "hidden":false};
+        
+        console.log(JSON.stringify(emptyObj.initialValues));
+      }
+  
+      function initialze() {
+          $(".SlidingPuzzle li.Tile").bind( "click", function() {
+  
+              emptyObj.clickedValue = parseInt($(this).text());
+              clickedArray = _.find(emptyObj.initialValues, {
+                  'value': parseInt($(this).text())
+              });
+              emptyObj.clickIndex = [clickedArray.row, clickedArray.column];
+  
+              emptyObj.emptyTiles = [];
+              emptyObj.emptyTiles = findEmptyTilePosition();
+              returnedValueEmptyTile = CheckIfEmptyTilePresentNear();
+              if (returnedValueEmptyTile == true) {
+                  updateInitialValuesObject();
+                  swapTiles();
+              }
+          });
+      }
+  
